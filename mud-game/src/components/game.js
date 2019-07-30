b4db0c989bf179b86b39fa1254612ae934b7a1b1
AmarachiOE <ao.ejiawoko@gmail.com>
2019-07-29 17:56:58 -0700

    post request to /move test server api. there's a lag, but it's working.

--- mud-game/src/components/game.js
+++ mud-game/src/components/game.js
@@ -10,7 +10,8 @@ class Game extends React.Component {
       name: "",
       title: "",
       description: "",
-      players: []
+      players: [],
+      move: ""
     };
   }
 
@@ -20,7 +21,7 @@ class Game extends React.Component {
 
     const headers = {
       headers: {
-        "Authorization": token,
+        Authorization: token,
         "Content-Type": "application/json"
       }
     };
@@ -42,15 +43,61 @@ class Game extends React.Component {
       });
   }
 
+  handleChanges = e => {
+    const { id, value } = e.target;
+    this.setState({ [id]: value });
+  };
+
+  playerMove = e => {
+    e.preventDefault();
+
+    // get button's id
+    const { id } = e.target;
+
+    // set this.state.move to button id
+    this.setState({ move: id });
+
+    console.log("Move on state: ", this.state.move.toString());
+
+    // create direction var that sets direction arg to this.state.move
+    const direction = {
+      direction: this.state.move
+      //   direction: id // this way updates automatically
+    };
+
+    // create token var, authorization, and content-type header arg
+    const token = "Token " + localStorage.getItem("key");
+    const headers = {
+      headers: {
+        Authorization: token,
+        "Content-Type": "application/json"
+      }
+    };
+
+    // axios call
+    const endpoint = this.props.baseUrl + "adv/move/";
+    axios
+      .post(endpoint, direction, headers)
+      .then(res => {
+        console.log(res.data);
+        this.setState({
+          uuid: res.data.uuid,
+          name: res.data.name,
+          title: res.data.title,
+          description: res.data.description,
+          players: res.data.players
+        });
+      })
+      .catch(err => {
+        console.log("Whoops! ", err);
+      });
+  };
+
   render() {
     return (
       <div>
         <h2>The Game</h2>
         <div>
-          {/* PLAYER DETAILS */}
-          <h3>Player Details:</h3>
-          <p>Username: {this.state.name}</p>
-
           {/* ROOM DETAILS */}
 
           <h3>Room Details:</h3>
@@ -65,6 +112,34 @@ class Game extends React.Component {
               <p>{player}</p>
             </div>
           ))}
+
+          {/* PLAYER DETAILS */}
+          <h3>Player Details:</h3>
+          <p>Username: {this.state.name}</p>
+          <p>You Entered: {this.state.move}</p>
+
+          <div className="direction-buttons">
+            <button id="n" onClick={this.playerMove}>
+              N
+            </button>
+            <button id="e" onClick={this.playerMove}>
+              E
+            </button>
+            <button id="s" onClick={this.playerMove}>
+              S
+            </button>
+            <button id="w" onClick={this.playerMove}>
+              W
+            </button>
+          </div>
+
+          <input
+            id="move"
+            type="text"
+            value={this.state.move}
+            onChange={this.handleChanges}
+            placeholder="what do you wanna do"
+          />
         </div>
       </div>
     );
