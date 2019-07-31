5bb528d9641e6645142987b93580f701e9752cbd
AmarachiOE <ao.ejiawoko@gmail.com>
2019-07-31 14:27:19 -0700

    added practice map JSX to see if it worked. it worked.

--- mud-game/src/components/Map.js
+++ mud-game/src/components/Map.js
@@ -2,16 +2,20 @@
 import React from "react";
 import picture from "../images/smileyRectangle.png";
 import PracticeMapJSX from "./PracticeMapJSX";
-const Map = props => {
-  return (
-    <div className="map-container">
-      {/* <img src={picture} alt="Smiley face" className="map"/> */}
+class Map extends React.Component {
 
-      <div className="map">
-        {PracticeMapJSX}
+  render() {
+    return (
+      <div className="map-container">
+        <img src={picture} alt="Smiley face" className="map"/>
+  
+        {/* <div className="map">
+          {PracticeMapJSX}
+        </div> */}
       </div>
-    </div>
-  );
+    );
+  }
+  
 };
 
 export default Map;
