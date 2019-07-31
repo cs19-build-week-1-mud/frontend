aee4a6fe739cb909c2f153344c374335ad1fbd12
AmarachiOE <ao.ejiawoko@gmail.com>
2019-07-31 13:56:02 -0700

    WIP. Experimenting with visualization algorithm

--- mud-game/src/components/Map.js
+++ mud-game/src/components/Map.js
@@ -1,11 +1,15 @@
 // Packages
 import React from "react";
 import picture from "../images/smileyRectangle.png";
-
+import PracticeMapJSX from "./PracticeMapJSX";
 const Map = props => {
   return (
     <div className="map-container">
-      <img src={picture} alt="Smiley face" className="map"/>
+      {/* <img src={picture} alt="Smiley face" className="map"/> */}
+
+      <div className="map">
+        {PracticeMapJSX}
+      </div>
     </div>
   );
 };
